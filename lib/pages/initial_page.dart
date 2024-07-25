2e88501b7df4ad60dca7d3d1706542f38bd834ed
pikybar <minnoli.raghavan@gmail.com>
2024-07-25 17:09:21 -0300

    made home page scrollable

--- lib/pages/initial_page.dart
+++ lib/pages/initial_page.dart
@@ -16,99 +16,101 @@ class InitialPage extends StatelessWidget {
         image: DecorationImage(image: AssetImage(backgroundImage), fit: BoxFit.cover, opacity: 0.25,)
       ),
       child: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 20,),
-            Image(image: AssetImage('assets/logo-white.png'), height: 130,),
-            SizedBox(height: 10,),
-            
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 5,
-                horizontal: 35
-              ),
-              child: Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      appState.setPage(CarrerasPage());
-                    }, 
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5),),
-                      backgroundColor: Colors.black,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center, 
-                      children: [Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text("Carreras".toUpperCase(), style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white)),
-                      )]
-                    ),
-                  ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 20,),
+              Image(image: AssetImage('assets/logo-white.png'), height: 130,),
+              SizedBox(height: 10,),
               
-                  SizedBox(
-                    height: 350,
-                    child: ListView(
-                      padding: EdgeInsets.zero,
-                      children: carreraCardsList,
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 35
+                ),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        appState.setPage(CarrerasPage());
+                      }, 
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5),),
+                        backgroundColor: Colors.black,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center, 
+                        children: [Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text("Carreras".toUpperCase(), style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white)),
+                        )]
+                      ),
                     ),
+                
+                    SizedBox(
+                      height: 350,
+                      child: ListView(
+                        padding: EdgeInsets.zero,
+                        children: carreraCardsList,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 35
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    appState.setPage(
+                      Login(directTo: "home")
+                    );
+                  }, 
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5),),
                   ),
-                ],
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 5,
-                horizontal: 35
-              ),
-              child: ElevatedButton(
-                onPressed: () {
-                  appState.setPage(
-                    Login(directTo: "home")
-                  );
-                }, 
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5),),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Home Corredor".toUpperCase(), style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white),),
+                      ),
+                    ],
+                  )
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text("Home Corredor".toUpperCase(), style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white),),
-                    ),
-                  ],
-                )
               ),
-            ),
-        
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 5,
-                horizontal: 35
-              ),
-              child: ElevatedButton(
-                onPressed: () {
-                  appState.setPage(Placeholder());
-                }, 
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5),),
-                  backgroundColor: Colors.black,
+          
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 35
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text("Expos Abiertas".toUpperCase(), style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white)),
-                    ),
-                  ],
-                )
+                child: ElevatedButton(
+                  onPressed: () {
+                    appState.setPage(Placeholder());
+                  }, 
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5),),
+                    backgroundColor: Colors.black,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Expos Abiertas".toUpperCase(), style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white)),
+                      ),
+                    ],
+                  )
+                ),
               ),
-            ),
-          ]
+            ]
+          ),
         ),
       ),
     );
